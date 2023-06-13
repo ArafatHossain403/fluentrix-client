27b6c15b65711277d000f2ddacb7fd9d93f0436d
Arafat Hossain <arafatnubt@gmail.com>
2023-06-13 18:38:53 +0600

    student dash board set

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,40 +1,48 @@
-import {
-    createBrowserRouter,
-    
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Classes from "../Pages/Classes/Classes";
 import Instructors from "../Pages/Instructors/Instructors";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import DashBoard from "../Layout/DashBoard";
+import StudentDashBoard from "../Pages/DashBoard/StudentDashBoard";
 
-
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-       {
-        path:'/',
-        element:<Home></Home>
-       },
-       {
-        path:'classes',
-        element:<Classes></Classes>,
-       },
-       {
-        path:'instructors',
-        element:<Instructors></Instructors>,
-       },
-       {
-        path:'login',
-        element:<Login></Login>,
-       },
-       {
-        path:'signup',
-        element:<SignUp></SignUp>,
-       }
-      ]
-    },
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "classes",
+        element: <Classes></Classes>,
+      },
+      {
+        path: "instructors",
+        element: <Instructors></Instructors>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: <DashBoard></DashBoard>,
+    children: [
+      {
+        path: "studentDashboard",
+        element: <StudentDashBoard></StudentDashBoard>,
+      },
+    ],
+  },
+]);
